fafeac813fec034cc3fe28d3e5f80cb5b47d810c
Arshak Tsarukyan <arshakt@gmail.com>
2022-06-22 17:03:50 +0400

    Added Path Params numberic validations

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Path, Query
 
 app = FastAPI()
 
 
-@app.get("/items/")
+@app.get("/items/{item_id}")
 async def read_items(
-    q: str
-    | None = Query(
-        default=None,
-        alias="item-query",
-        title="Query string",
-        description="Query string for the items to search in the database that have a good match",
-        min_length=3,
-        max_length=50,
-        regex="^fixedquery$",
-        deprecated=True,
-    )
+    *,
+    item_id: int = Path(title="The ID of the item to get", ge=0, le=1000),
+    q: str,
+    size: float = Query(gt=0, lt=10.5)
 ):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    results = {"item_id": item_id}
     if q:
         results.update({"q": q})
     return results
