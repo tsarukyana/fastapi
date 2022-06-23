d1c9d204ba0a744cfb68ac5c37c5a7697fe673fe
Arshak Tsarukyan <arshakt@gmail.com>
2022-06-23 09:50:37 +0400

    Added Form Data

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, Form
 
 app = FastAPI()
 
 
-@app.post("/items/", status_code=status.HTTP_201_CREATED)
-async def create_item(name: str):
-    return {"name": name}
+@app.post("/login/")
+async def login(username: str = Form(), password: str = Form()):
+    return {"username": username}
