13edd2d9a8279a7fdf329ad3a98bb59d6714bdd2
Arshak Tsarukyan <arshakt@gmail.com>
2022-06-23 10:52:26 +0400

    Added Security - First Steps

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-from fastapi import Depends
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
+
+app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-async def dependency_a():
-    dep_a = generate_dep_a()
-    try:
-        yield dep_a
-    finally:
-        dep_a.close()
-
-
-async def dependency_b(dep_a=Depends(dependency_a)):
-    dep_b = generate_dep_b()
-    try:
-        yield dep_b
-    finally:
-        dep_b.close(dep_a)
-
-
-async def dependency_c(dep_b=Depends(dependency_b)):
-    dep_c = generate_dep_c()
-    try:
-        yield dep_c
-    finally:
-        dep_c.close(dep_b)
-
+@app.get("/items/")
+async def read_items(token: str = Depends(oauth2_scheme)):
+    return {"token": token}
